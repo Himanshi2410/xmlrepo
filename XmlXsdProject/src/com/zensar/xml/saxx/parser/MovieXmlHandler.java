34a237a436344cc558265eb32900ff6256f0df8e
himanshi2410 <himanshikumrawat95@gmail.com>
2019-07-21 18:42:42 -0700

    MovieXML.java created

--- XmlXsdProject/src/com/zensar/xml/saxx/parser/MovieXmlHandler.java
+++ XmlXsdProject/src/com/zensar/xml/saxx/parser/MovieXmlHandler.java
@@ -19,25 +19,25 @@ public class MovieXmlHandler extends DefaultHandler {
 	@Override
 	public void endDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.endDocument();
+		System.out.println("end od xmlm document");
 	}
 
 	@Override
 	public void endElement(String arg0, String arg1, String arg2) throws SAXException {
 		// TODO Auto-generated method stub
-		super.endElement(arg0, arg1, arg2);
+		System.out.println("end element:" +arg2);
 	}
 
 	@Override
 	public void startDocument() throws SAXException {
 		// TODO Auto-generated method stub
-		super.startDocument();
+		System.out.println("starting....");
 	}
 
 	@Override
 	public void startElement(String arg0, String arg1, String arg2, Attributes arg3) throws SAXException {
 		// TODO Auto-generated method stub
-		super.startElement(arg0, arg1, arg2, arg3);
+		System.out.println("start element:" +arg2);
 	}
 	
 	
